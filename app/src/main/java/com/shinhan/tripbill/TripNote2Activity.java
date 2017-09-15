532e5675b05fd72f09850671ad56f972e0d9d7fa
Jeong Seon Kim <newnafhnafh@gmail.com>
2017-09-15 15:40:20 +0900

    여행노트 화면 두번째 추가'

--- app/src/main/java/com/shinhan/tripbill/TripNote2Activity.java
+++ app/src/main/java/com/shinhan/tripbill/TripNote2Activity.java
@@ -3,11 +3,11 @@ package com.shinhan.tripbill;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TripNote extends AppCompatActivity {
+public class TripNote2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip_note);
+        setContentView(R.layout.activity_trip_note2);
     }
 }
