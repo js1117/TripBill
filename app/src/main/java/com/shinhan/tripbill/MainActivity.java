c814eef7ae653cb2af035cfb73753191c436d132
Jeong Seon Kim <newnafhnafh@gmail.com>
2017-09-22 17:23:14 +0900

    화면전환

--- app/src/main/java/com/shinhan/tripbill/MainActivity.java
+++ app/src/main/java/com/shinhan/tripbill/MainActivity.java
@@ -1,7 +1,9 @@
 package com.shinhan.tripbill;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void note1(View view) {
+        Intent intent = new Intent(MainActivity.this, TripNote1Activity.class);
+        startActivity(intent);
+    }
+    public void note2(View view) {
+        Intent intent = new Intent(MainActivity.this, TripNote2Activity.class);
+        startActivity(intent);
     }
 }
